7bd333b4d700e42173ccfae280c0e347ec5f78ee
cmgcookie <armin1213@hotmail.com>
2016-11-10 14:13:16 -0600

    Agregados cambios para administrador

--- alumnos/datos/admin.py
+++ alumnos/datos/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import infoGeneral
+
+class infoGeneralAdmin(admin.ModelAdmin):
+    pass
+
+admin.site.register(infoGeneral, infoGeneralAdmin)
