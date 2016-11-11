d5093bb4b93d433f7755d2979772366ba8604a64
cmgcookie <armin1213@hotmail.com>
2016-11-11 13:36:09 -0600

    Agregados templates

--- alumnos/datos/views.py
+++ alumnos/datos/views.py
@@ -3,5 +3,10 @@ from django.http import HttpResponse
 
 
 def hola(request):
-    return HttpResponse("Hello World from Django")
+    return render(request, "index.html", {"nombre":"Cookie Monster"})
+    #return HttpResponse("Hello World from Django")
+
+def formulario(request):
+    form = "Hola aquí el formulario"
+    return render(request, "formulario.html", {"formulario":form})
 # Create your views here.
